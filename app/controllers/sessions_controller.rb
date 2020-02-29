337408cfc7e18b7d737284339fc53f11b0c75b93
keisuke.kato <kgulliverk@hotmail.com>
2020-02-29 19:28:29 +0900

    log in　メソッド作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find by(email: params[:session][:email].douncase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirecr_to user_path(user.id)
+    else
+      flash[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
 end
